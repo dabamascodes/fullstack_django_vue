e2e3f2a00c805de44f3b35dcc3e93bd9db23afe2
dabamascodes <daniel.6@live.com.mx>
2025-02-21 18:30:30 -0700

    Creación método POST y validación de campos

--- backend/contacto/views.py
+++ backend/contacto/views.py
@@ -8,5 +8,15 @@ class Clase1(APIView):
     
     
     def post(self, request):
-        pass
-
+        if request.data.get("nombre")==None or not request.data['nombre']:
+            return JsonResponse({"estado":"error", "mensaje":"El campo nombre es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("correo")==None or not request.data['correo']:
+            return JsonResponse({"estado":"error", "mensaje":"El campo correo es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("telefono")==None or not request.data['telefono']:
+            return JsonResponse({"estado":"error", "mensaje":"El campo telefono es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("mensaje")==None or not request.data['mensaje']:
+            return JsonResponse({"estado":"error", "mensaje":"El campo mensaje es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
