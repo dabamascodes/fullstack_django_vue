a2f2f17ae4d38e27eb07c8ecfe9bc47e0e4ceb70
dabamascodes <daniel.6@live.com.mx>
2025-03-05 07:54:47 -0700

    Validar que no exista correo antes de registrar

--- backend/seguridad/views.py
+++ backend/seguridad/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from django.http.response import JsonResponse
 from django.http import Http404
 from http import HTTPStatus
+from django.contrib.auth.models import User
 
 from .models import *
 
@@ -12,9 +13,12 @@ class Clase1(APIView):
     def post(self, request):
         if request.data.get("nombre")==None or not request.data.get("nombre"):
             return JsonResponse({"estado":"error", "mensaje":"El campo nombre es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
-        
         if request.data.get("correo")==None or not request.data.get("correo"):
             return JsonResponse({"estado":"error", "mensaje":"El campo correo es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
-        
         if request.data.get("password")==None or not request.data.get("password"):
             return JsonResponse({"estado":"error", "mensaje":"El campo password es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
+        
+        if User.objects.filter(email=request.data["correo"]).exists():
+            return JsonResponse({"estado":"error", "mensaje":f"El correo {request.data["correo"]} no está disponible"}, status=HTTPStatus.BAD_REQUEST)
+        
