3eaba5b5461d9aa9ad775fe4bb16694d4e628b25
dabamascodes <daniel.6@live.com.mx>
2025-03-11 08:20:41 -0600

    Creación de decorador personalizado - @wraps

---
+++ backend/seguridad/decorators.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from django.http.response import JsonResponse
+from http import HTTPStatus
+from jose import jwt
+from django.conf import settings
+import time
+
+def logueado():
+    def metodo(func):
+        @wraps(func)
+        def _decorator(request, *args, **kwars):
+            req = args[0]
+            if not req.headers.get('Authorization') or req.headers.get('Authorization')==None:
+                return JsonResponse({"estado":"error", "mensaje":"Sin autorización"}, status=HTTPStatus.UNAUTHORIZED)
+            header = req.headers.get('Authorization').split(" ")
+            try:
+                resuelto=jwt.decode(header[1], settings.SECRET_KEY, algorithms=['HS512'])
+            except Exception as e:
+                return JsonResponse({"estado":"error", "mensaje":"Sin autorización"}, status=HTTPStatus.UNAUTHORIZED)
+            
+            if int(resuelto["exp"])>int(time.time()):
+                return func(request, *args, **kwars)
+            else:
+                return JsonResponse({"estado":"error", "mensaje":"Sin autorización"}, status=HTTPStatus.UNAUTHORIZED)
+
+        return _decorator
+    return metodo
+                
