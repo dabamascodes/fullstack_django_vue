a2e8a55fb0290f617381db28d9775f97ef33cc4b
dabamascodes <daniel.6@live.com.mx>
2025-03-14 08:39:24 -0600

    Tarea - Agregar Validación - Sí el user is active

--- backend/seguridad/decorators.py
+++ backend/seguridad/decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from django.http.response import JsonResponse
+from django.contrib.auth.models import User
 from http import HTTPStatus
 from jose import jwt
 from django.conf import settings
@@ -15,7 +16,21 @@ def logueado():
             header = req.headers.get('Authorization').split(" ")
             try:
                 # Agregar Validación para comprobar sí el usuario está activo
-                resuelto=jwt.decode(header[1], settings.SECRET_KEY, algorithms=['HS512'])
+                resuelto=jwt.decode(header[1], settings.SECRET_KEY, algorithms=['HS512'])  
+                # Tomamos el user_id del JWT, y vamos a la tabla de auth_user a comprobar sí existe y si está activo el usuario
+                # Sí NO ESTÁ ACTIVO ó NO EXISTE, devolvemos un UNAUTHORIZED
+                # Como nmos damos cuenta, se puede generar el token correctamente, pero se pudiera manipular el user en la tabla y con eso se accederia
+                # Es por ello que agregamos este filtro al final de la validación
+                user_id = resuelto.get("id")
+                user = User.objects.filter(id=user_id).first()
+
+                if not user:
+                    # print('NOT EXISTS')
+                    return JsonResponse({"estado":"error", "mensaje":"No autorizado"}, status=HTTPStatus.UNAUTHORIZED)
+                if not user.is_active:
+                    # print('IS INACTIVE')
+                    return JsonResponse({"estado":"error", "mensaje":"No autorizado"}, status=HTTPStatus.UNAUTHORIZED)
+                
             except Exception as e:
                 return JsonResponse({"estado":"error", "mensaje":"No autorizado"}, status=HTTPStatus.UNAUTHORIZED)
             
