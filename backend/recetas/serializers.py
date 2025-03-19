5757481e7faacb44d1521c583bc2cbe63896a2be
dabamascodes <daniel.6@live.com.mx>
2025-03-19 06:57:28 -0600

    87. Agregar user a serializers recetas

--- backend/recetas/serializers.py
+++ backend/recetas/serializers.py
@@ -9,12 +9,26 @@ class RecetaSerializer(serializers.ModelSerializer):
     # categoria = serializers.CharField(source='categoria.nombre')
     fecha = serializers.DateTimeField(format="%d/%m/%Y") # 13/10/2025
     imagen = serializers.SerializerMethodField()
+    user = serializers.ReadOnlyField(source = 'user.first_name')
+    
     
     class Meta:
         model = Receta
-        fields = ("id", "nombre", "slug", "tiempo", "descripcion", "fecha", "categoria", "categoria_id", "imagen")
+        fields = (
+            "id", 
+            "nombre", 
+            "slug", 
+            "tiempo", 
+            "descripcion", 
+            "fecha", 
+            "categoria", 
+            "categoria_id", 
+            "imagen",
+            "user_id",
+            "user"
+        )
         
-        
+          
     def get_imagen(self, obj):
         load_dotenv()
         return f"{os.getenv("BASE_URL")}uploads/recetas/{obj.foto}"
